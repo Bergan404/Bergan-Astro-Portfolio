3522194299bfd6772d2bee01fae74caf498f4523
Bergan Oudshoorn <73203015+Bergan404@users.noreply.github.com>
2024-09-05 10:44:00 -0400

    Astro Deploy to Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue(), tailwind()]
+    integrations: [vue(), tailwind()],
+    site: 'https://berganoudshoorn.net',
 });
